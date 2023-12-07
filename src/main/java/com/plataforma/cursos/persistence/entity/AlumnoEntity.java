7bfa182e6999d01f7a0763c21d37b5d245de1231
MarioMozo <mario.psy@live.com>
2023-12-06 19:27:15 -0600

    Creacion de Gettes y Setters AlumnoEntity

--- src/main/java/com/plataforma/cursos/persistence/entity/AlumnoEntity.java
+++ src/main/java/com/plataforma/cursos/persistence/entity/AlumnoEntity.java
@@ -27,6 +27,43 @@ public class AlumnoEntity {
     @Column(nullable = false)
     private Date fechaNacimiento;
 
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
 
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
